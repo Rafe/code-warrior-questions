ee0519961d556adeb14635e03543cbca99dd3dd8
Jimmy Chao <daizenga@gmail.com>
2013-05-03 06:35:19 -0700

    fix answer

--- basic/4/answer.js
+++ basic/4/answer.js
@@ -18,3 +18,59 @@ var postorder = exports.postorder = function (head, tracker) {
   postorder(head.right, tracker);
   tracker.push(head.data);
 }
+
+var iterPreorder = function (head, tracker) {
+  var stack = [head];
+
+  while (stack.length) {
+    var node = stack.pop();
+    tracker.push(node.data);
+
+    if (node.right)
+      stack.push(node.right);
+    if (node.left)
+      stack.push(node.left);
+  }
+}
+
+var iterInorder = function (head, tracker) {
+  var stack = [head];
+
+  while (stack.length) {
+    var node = stack.pop();
+
+    if (node.visited) {
+      tracker.push(node.data);
+      continue;
+    } else {
+      node.visited = true;
+    };
+
+    if (node.right)
+      stack.push(node.right);
+    stack.push(node);
+    if (node.left)
+      stack.push(node.left);
+  }
+}
+
+var iterPostorder = function (head, tracker) {
+  var stack = [head];
+
+  while (stack.length) {
+    var node = stack.pop();
+
+    if (node.visited) {
+      tracker.push(node.data);
+      continue;
+    } else {
+      node.visited = true;
+    };
+
+    stack.push(node);
+    if (node.right)
+      stack.push(node.right);
+    if (node.left)
+      stack.push(node.left);
+  }
+}
--- basic/7/index.js
+++ basic/7/index.js
@@ -1,17 +1,5 @@
 var insert = exports.insert = function (array, index, value) {
-  var i = index - 1;
-  while (i >= 0 && array[i] > value) {
-    array[i + 1] = array[i];
-    i--;
-  }
-  array[i + 1] = value;
-
-  return array;
 }
 
 exports.sort = function (array) {
-  for (var i = 0; i < array.length; i++) {
-    insert(array, i, array[i]);
-  };
-  return array;
 }
